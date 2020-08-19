718968ee5187c20a81e33609fb05ba61cbfc2359
Andre Soares <andregsilv@gmail.com>
2020-08-19 03:07:35 -0300

    onpublish

--- auth-server/onpublish.go
+++ auth-server/onpublish.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,16 +9,11 @@ import (
 var STREAMING_KEY string
 
 func onpublish(c *gin.Context) {
-	var b bytes.Buffer
-	_, _ = b.ReadFrom(c.Request.Body)
-	fmt.Println("Payload:", b.String())
-	fmt.Println(c.Request.Header)
-	key := c.DefaultPostForm("name", "no-name-found")
+	c.Request.ParseForm()
+	key := c.PostForm("name")
 	if key == STREAMING_KEY {
-		fmt.Printf("good key %s == %s\n", key, STREAMING_KEY)
 		c.String(http.StatusOK, "Good to go")
 	} else {
-		fmt.Printf("bad key %s != %s\n", key, STREAMING_KEY)
 		c.String(http.StatusUnauthorized, "Invalid straming key")
 	}
 }
